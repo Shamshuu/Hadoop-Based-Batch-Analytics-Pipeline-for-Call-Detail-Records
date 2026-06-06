7ba632cc8e4a3504b2766ffc145e15255ce5f231
Shamshuu <shamshu2004@gmail.com>
2026-06-06 16:48:58 +0530

    feat: implement end-to-end Hadoop-based batch analytics pipeline with Spark jobs and Airflow DAGs for CDR processing

---
+++ dags/revenue_recon_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2026, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=10),
+}
+
+with DAG(
+    'revenue_reconciliation_dag',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    submit_spark_job = BashOperator(
+        task_id='submit_spark_job',
+        bash_command=(
+            "spark-submit --master spark://spark-master:7077 "
+            "/jobs/revenue_recon.py "
+            "--run-id \"{{ dag_run.conf.get('run_id', 'manual_run') }}\" "
+            "--input /data/cdr_data.csv"
+        )
+    )
